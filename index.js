df590f8f84727e65bdc2099942a71b9d766f7f91
creatorm22 <mikautadze.giorgi2223@gmail.com>
2024-01-30 11:46:57 +0400

    added data.json file and deleted index.html

--- index.js
+++ index.js
@@ -12,101 +12,119 @@ const fs = require("fs").promises;
 
 const app = express();
 
-let expenseManager = [
-  {
-    id: 1,
-    name: "Groceries",
-    cost: 200,
-    createdAt: "2021-01-01",
-  },
-  {
-    id: 2,
-    name: "Rent",
-    cost: 1000,
-    createdAt: "2021-01-01",
-  },
-];
+async function readExpenseManager() {
+  try {
+    const data = await fs.readFile("./data.json", "utf-8");
+    const expenseManager = JSON.parse(data);
+    console.log(expenseManager);
+    return expenseManager;
+  } catch (error) {
+    console.error("Error reading file:", error);
+    throw error;
+  }
+}
 
 app.use(express.json());
 
-app.get("/api/expenses", (req, res) => {
-  res.json({ success: true, data: expenseManager });
-});
-
-app.get("/api/expenses/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const index = expenseManager.findIndex(
-    (expense) => expense.id === parseInt(id)
-  );
-
-  if (index === -1) {
-    res.status(404);
-    res.json({ success: false, message: "Expense not found" });
+app.get("/api/expenses", async (req, res) => {
+  try {
+    const expenseManager = await readExpenseManager();
+    res.json({ success: true, data: expenseManager });
+  } catch (error) {
+    res.json({ success: false, message: "Error" });
   }
-
-  const htmlContent = `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Document</title>
-    </head>
-    <body>
-      <p>${JSON.stringify(expenseManager[index])}</p>
-    </body>
-  </html>`;
-
-  res.setHeader("Content-Type", "text/html");
-
-  res.send(htmlContent);
 });
 
-app.post("/api/expenses", (req, res) => {
-  const expense = req.body;
-  const lastId = expenseManager[expenseManager.length - 1]?.id;
-  expense.id = lastId ? lastId + 1 : 1;
-  expenseManager.push(expense);
-  res.send({ success: true, data: expenseManager, message: "expense added" });
-});
+app.get("/expenses/:id", async (req, res) => {
+  try {
+    const expenseManager = await readExpenseManager();
+    const id = parseInt(req.params.id);
+    const index = expenseManager.findIndex((expense) => expense.id === id);
 
-app.delete("/api/expenses/:id", (req, res) => {
-  const { id } = req.params;
-  console.log(id);
+    if (index === -1) {
+      res.status(404).json({ success: false, message: "Expense not found" });
+    }
 
-  const index = expenseManager.findIndex(
-    (expense) => expense.id === parseInt(id)
-  );
+    const expense = expenseManager[index];
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Expense Details</title>
+        </head>
+        <body>
+          <p>${JSON.stringify(expense)}</p>
+        </body>
+      </html>`;
 
-  if (index === -1) {
-    res.status(404);
-    res.json({ success: false, message: "Expense doesn't exists" });
+    res.setHeader("Content-Type", "text/html");
+    res.send(htmlContent);
+  } catch (error) {
+    res.json({ success: false, message: "Error" });
   }
-  const expense = expenseManager[index];
-  expenseManager = expenseManager.filter(
-    (expense) => expense.id !== parseInt(id)
-  );
-
-  res.json({ success: true, data: expense, message: "expense deleted" });
-  console.log(expenseManager);
 });
 
-app.put("/api/expenses/:id", (req, res) => {
-  const changeExpense = req.body;
-  const { id } = req.params;
-  const index = expenseManager.findIndex((exp) => exp.id === parseInt(id));
-  if (index === -1) {
-    res.status(404);
-    res.json({ success: false, message: "Expense not found" });
+app.post("/api/expenses", async (req, res) => {
+  try {
+    const expenseManager = await readExpenseManager();
+    const expense = req.body;
+    const lastId = expenseManager[expenseManager.length - 1]?.id;
+    expense.id = lastId ? lastId + 1 : 1;
+    expenseManager.push(expense);
+    res.send({ success: true, data: expenseManager, message: "expense added" });
+  } catch (error) {
+    res.json({ success: false, message: "Error" });
   }
-  let expense = expenseManager[index];
-  expense = {
-    ...expense,
-    ...changeExpense,
-  };
-  expenseManager[index] = expense;
-  res.json({ success: true, data: expense, message: "expense changed" });
+});
+
+app.delete("/api/expenses/:id", async (req, res) => {
+  try {
+    const expenseManager = await readExpenseManager();
+    const { id } = req.params;
+    console.log(id);
+
+    const index = expenseManager.findIndex(
+      (expense) => expense.id === parseInt(id)
+    );
+
+    if (index === -1) {
+      res.status(404);
+      res.json({ success: false, message: "Expense doesn't exists" });
+    }
+    const expense = expenseManager[index];
+    expenseManager = expenseManager.filter(
+      (expense) => expense.id !== parseInt(id)
+    );
+
+    res.json({ success: true, data: expense, message: "expense deleted" });
+    console.log(expenseManager);
+  } catch (error) {
+    res.json({ success: false, message: "Error" });
+  }
+});
+
+app.put("/api/expenses/:id", async (req, res) => {
+  try {
+    const expenseManager = await readExpenseManager();
+    const changeExpense = req.body;
+    const { id } = req.params;
+    const index = expenseManager.findIndex((exp) => exp.id === parseInt(id));
+    if (index === -1) {
+      res.status(404);
+      res.json({ success: false, message: "Expense not found" });
+    }
+    let expense = expenseManager[index];
+    expense = {
+      ...expense,
+      ...changeExpense,
+    };
+    expenseManager[index] = expense;
+    res.json({ success: true, data: expense, message: "expense changed" });
+  } catch (error) {
+    res.json({ success: false, message: "Error" });
+  }
 });
 
 app.listen(3000, () => {
